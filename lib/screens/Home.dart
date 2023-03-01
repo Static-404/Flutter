0277830562ba07429c13d41b2e0948068ea43d26
praise-lab <pboybams16@gmail.com>
2023-03-01 21:13:20 +0100

    Day 3
    Added a page view
    Added 3 pages to the page view
    Added the SmoothPageIndicator
    added a skip text
    added an arrow icon that changes to a done text at the end of the page
    added images and color to the pages
    changed the appbar text to Welcome

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,14 +9,64 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  PageController _controller = PageController();
+  bool onLastPage = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
-          title: const Text("Home"),
+          title: const Text("Welcome"),
           centerTitle: true,
         ),
+       body: Stack(
+           children: [
+             PageView(
+               controller: _controller,
+             onPageChanged: (index){
+                 setState(() {
+                   onLastPage = (index == 2);
+                   print("$index, $onLastPage");
+                 });
+             },
+             children: [
+               Container(
+                 color: Colors.lightGreen,
+                 child: Image.asset('assets/images/book.jpg'),
+               ),
+               Container(
+                 color: Colors.lightGreen,
+                 child: Image.asset('assets/images/library.jpg'),
+               ),
+               Container(
+                 color: Colors.lightGreen,
+                 child: Image.asset('assets/images/reading.jpg'),
+               )
+             ],
+           ),
+             Container(
+               alignment: const Alignment(0,0.85),
+               child: Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                 children: [
+                   const Text('Skip'),
+                   SmoothPageIndicator(
+                     controller: _controller,
+                     count: 3,
+                     effect: const SwapEffect(
+                       dotColor: Colors.blue,
+                       activeDotColor: Colors.white,
+                       type: SwapType.yRotation,
+                     ),
+                   ),
+                   onLastPage ? const Text('Done') : const Icon(Icons.arrow_forward),
+                 ],
+               ),
+             )
+           ],
+       )
+      // Center(child: Text("Hello Guys"),),
       );
   }
 }
