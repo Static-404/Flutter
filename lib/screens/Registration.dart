c5ff435a35fd06cd0edcdc4d21b6a408f2c74151
praise-lab <pboybams16@gmail.com>
2023-03-06 22:30:48 +0100

    Day 8
    modified the registration page
    added a boder to the container and the text field
    added a box shadow
    added a boder radius

--- lib/screens/Registration.dart
+++ lib/screens/Registration.dart
@@ -11,49 +11,66 @@ class Registration extends StatelessWidget {
         title: const Text("Registration Page"),
         centerTitle: true,
       ),
-      body: Container(
-        padding: const EdgeInsets.all(30),
-        color: Colors.lightGreenAccent,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Container(
-                height: 100,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: NetworkImage("https://i.pravatar.cc/150?u=fake@pravatar.com"),),
-                )
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 550,
+          decoration: BoxDecoration(
+            color: Colors.lightGreenAccent,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: const [
+              BoxShadow(
+                  color: Colors.black54,
+                  offset: Offset(5.0,-5.0,))
+            ],
+            border: Border.all(
+              color: Colors.blueGrey,
+              width: 5,
             ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: "User Name", hintStyle: TextStyle(color: Colors.black)),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(hintText: "Email", hintStyle: TextStyle(color: Colors.black)),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: const InputDecoration(hintText: "Password", hintStyle: TextStyle(color: Colors.black)),
-            ),
-            MaterialButton(onPressed: (){},
-              color: Colors.lightBlue,
-              child: const Text("Register", style: TextStyle(color: Colors.black),),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  width: 50,
+          ),
+          padding: const EdgeInsets.all(30),
+          // color: Colors.lightGreenAccent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
                   height: 100,
-                  color: Colors.black12,
-                ),
-                Container(
-                  width: 50,
-                  height: 100,
-                  color: Colors.black12,
-                )
-              ],
-            ),
-          ],
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(image: NetworkImage("https://i.pravatar.cc/150?u=fake@pravatar.com"),),
+                  )
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: "User Name", hintStyle: TextStyle(color: Colors.black), border: OutlineInputBorder(),),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(hintText: "Email", hintStyle: TextStyle(color: Colors.black),border: OutlineInputBorder(),),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(hintText: "Password", hintStyle: TextStyle(color: Colors.black),border: OutlineInputBorder(),),
+              ),
+              MaterialButton(onPressed: (){},
+                color: Colors.lightBlue,
+                child: const Text("Register", style: TextStyle(color: Colors.black),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 50,
+                    height: 20,
+                    color: Colors.black12,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 20,
+                    color: Colors.black12,
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
