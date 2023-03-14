203e4737af2273c0c848d5251af848dd5b4f072b
praise-lab <pboybams16@gmail.com>
2023-03-14 22:39:47 +0100

    Day 16
    added a floating button and a popup widget
    added a state to the textfield onsubmit and onchanged
    added a future builder and a circular progress indicator to the taskwidget

--- lib/screens/todo.dart
+++ lib/screens/todo.dart
@@ -1,22 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TaskPage extends StatefulWidget {
   const TaskPage({Key? key}) : super(key: key);
-
   @override
   State<TaskPage> createState() => _TaskPageState();
 }
 
 class _TaskPageState extends State<TaskPage> {
+
+  double? _deviceHeight, _deviceWidth;
+  String? content;
+  Box? _box;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: _deviceHeight! * 0.1,
+        title: const Text('Daily Planner'),
 
       ),
-      body: Center(
-        child: const Text('TodoPage'),
+      body: _tasksWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: displayTaskPop,
+        child: const Icon(Icons.add),
       ),
     );
   }
+  Widget _tasksWidget(){
+    return FutureBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+      if(snapshot.hasData){
+        _box = snapshot.data;
+        return Center();
+      }else{
+        return Center(child: const CircularProgressIndicator());
+      }
+    });
+  }
+
+  displayTaskPop(){
+    showDialog(context: context, builder: (BuildContext _context){
+      return AlertDialog(
+        title: Text('Add a note'),
+        content: TextField(
+          onSubmitted: (value){
+            print(value);
+            Navigator.pop(context);
+          },
+          onChanged: (value){
+            setState(() {
+              content = value;
+              print(value);
+            });
+          },
+        ),
+      );
+    });
+  }
+
 }
