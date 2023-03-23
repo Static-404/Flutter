d63c9050956bb1113400698aa4e3aff1abb80363
praise-lab <pboybams16@gmail.com>
2023-03-23 23:40:50 +0100

    Day 25
    added a divider to the second list page and navigated the lists to another page

--- lib/screens/secondpage.dart
+++ lib/screens/secondpage.dart
@@ -30,73 +30,115 @@ class _SecondPageState extends State<SecondPage> {
       body: Container(
         color: Colors.greenAccent,
         child: ListView(
-            children: const [
+          scrollDirection: Axis.vertical,
+            children: ListTile.divideTiles(context:context,
+              color: Colors.blueAccent,
+              tiles:[
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, 'todo');
+                },
                 leading: Icon(Icons.list),
                 title: Text('one',),
                 subtitle: Text('Book 1'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, 'todo');
+                },
                 leading: Icon(Icons.list),
                 title: Text('two',),
                 subtitle: Text('Book 2'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('three',),
                 subtitle: Text('Book 3'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('four',),
                 subtitle: Text('Book 4'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('five',),
                 subtitle: Text('Book 5'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('six',),
                 subtitle: Text('Book 6'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('seven',),
                 subtitle: Text('Book 7'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('eight',),
                 subtitle: Text('Book 8'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('nine',),
                 subtitle: Text('Book 9'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('Ten',),
                 subtitle: Text('Book 10'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('eleven',),
                 subtitle: Text('Book 11'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('twelve',),
                 subtitle: Text('Book 12'),
               ),
               ListTile(
+                onTap: (){
+                  Navigator.pushNamed(context, '1Page');
+                },
                 leading: Icon(Icons.list),
                 title: Text('thirteen',),
                 subtitle: Text('Book 13'),
               ),
-            ],
+            ]).toList(),
           ),
       ),
     );
